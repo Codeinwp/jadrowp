12ce54af71617ead6d1e3409d40b20878676d5a2
Manesh Timilsina <smanesh2004@gmail.com>
2023-05-04 17:54:52 +0545

    Testimonials pattern update

--- patterns/testimonials.php
+++ patterns/testimonials.php
@@ -11,29 +11,49 @@
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"8rem","bottom":"8rem","right":"20px","left":"20px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:8rem;padding-right:20px;padding-bottom:8rem;padding-left:20px"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:columns -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":"bottom"} -->
+<div class="wp-block-columns are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"color":{"text":"#e06035"}},"className":"has-text-align-left","fontSize":"medium"} -->
+<p class="has-text-align-left has-text-color has-medium-font-size" style="color:#e06035;font-style:normal;font-weight:600"><?php esc_html_e( 'Customer Reviews', 'jadro' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"left","fontSize":"max-60"} -->
+<h2 class="wp-block-heading has-text-align-left has-max-60-font-size"><?php esc_html_e( 'Testimonials', 'jadro' ); ?></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"0.75rem"}}}} -->
+<p style="margin-bottom:0.75rem"><?php esc_html_e( 'A range of testimonials from our happy customers across a variety of industries and use cases.', 'jadro' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"2rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:2rem"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"backgroundColor":"white"} -->
 <div class="wp-block-column has-white-background-color has-background"><!-- wp:group {"style":{"spacing":{"padding":{"top":"2.5rem","right":"2.5rem","bottom":"2.5rem","left":"2.5rem"}},"border":{"color":"#e7e7e7","width":"1px","radius":"10px"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color" style="border-color:#e7e7e7;border-width:1px;border-radius:10px;min-height:100%;padding-top:2.5rem;padding-right:2.5rem;padding-bottom:2.5rem;padding-left:2.5rem"><!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"align":"wide","style":{"spacing":{"margin":{"right":"0px","left":"0px"},"blockGap":{"top":"20px","left":"20px"}}}} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center is-not-stacked-on-mobile" style="margin-right:0px;margin-left:0px"><!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:image {"id":500,"sizeSlug":"thumbnail","linkDestination":"none","style":{"border":{"radius":"100%"}},"className":"is-style-rounded"} -->
-<figure class="wp-block-image size-thumbnail has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/headshot.png' ); ?>" alt="" class="wp-image-500" style="border-radius:100%"/></figure>
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:image {"id":305,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100%"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/image-1.jpg' ); ?>" alt="" class="wp-image-305" style="border-radius:100%"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"75%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:75%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
-<h2 class="wp-block-heading" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><?php esc_html_e( 'Eliza Weber', 'jadro' ); ?></h2>
+<h2 class="wp-block-heading" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Eliza Weber</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><?php esc_html_e( 'Life Coach', 'jadro' ); ?></p>
+<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Life Coach</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
 <!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left"><?php esc_html_e( 'Simple, Clean &amp; minimal Full Site Editing theme.', 'jadro' ); ?></p>
+<p class="has-text-align-left"><?php esc_html_e( 'Simple, clean &amp; minimal Full Site Editing theme.', 'jadro' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"10px","right":"0px","bottom":"0px","left":"0px"}}},"textColor":"primary"} -->
@@ -46,18 +66,18 @@
 <div class="wp-block-column has-white-background-color has-background"><!-- wp:group {"style":{"spacing":{"padding":{"top":"2.5rem","right":"2.5rem","bottom":"2.5rem","left":"2.5rem"}},"border":{"color":"#e7e7e7","width":"1px","radius":"10px"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color" style="border-color:#e7e7e7;border-width:1px;border-radius:10px;min-height:100%;padding-top:2.5rem;padding-right:2.5rem;padding-bottom:2.5rem;padding-left:2.5rem"><!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"align":"wide","style":{"spacing":{"margin":{"right":"0px","left":"0px"},"blockGap":{"top":"20px","left":"20px"}}}} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center is-not-stacked-on-mobile" style="margin-right:0px;margin-left:0px"><!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:image {"id":500,"sizeSlug":"thumbnail","linkDestination":"none","style":{"border":{"radius":"100%"}},"className":"is-style-rounded"} -->
-<figure class="wp-block-image size-thumbnail has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/headshot.png' ); ?>" alt="" class="wp-image-500" style="border-radius:100%"/></figure>
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:image {"id":302,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100%"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/image-2.jpg' ); ?>" alt="" class="wp-image-302" style="border-radius:100%"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"75%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:75%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
-<h2 class="wp-block-heading" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><?php esc_html_e( 'Danial Forbes', 'jadro' ); ?></h2>
+<h2 class="wp-block-heading" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Danial Forbes</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><?php esc_html_e( 'Public Speaker', 'jadro' ); ?></p>
+<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Public Speaker</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
@@ -76,18 +96,18 @@
 <div class="wp-block-column has-white-background-color has-background"><!-- wp:group {"style":{"spacing":{"padding":{"top":"2.5rem","right":"2.5rem","bottom":"2.5rem","left":"2.5rem"}},"border":{"color":"#e7e7e7","width":"1px","radius":"10px"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color" style="border-color:#e7e7e7;border-width:1px;border-radius:10px;min-height:100%;padding-top:2.5rem;padding-right:2.5rem;padding-bottom:2.5rem;padding-left:2.5rem"><!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"align":"wide","style":{"spacing":{"margin":{"right":"0px","left":"0px"},"blockGap":{"top":"20px","left":"20px"}}}} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center is-not-stacked-on-mobile" style="margin-right:0px;margin-left:0px"><!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:image {"id":500,"sizeSlug":"thumbnail","linkDestination":"none","style":{"border":{"radius":"100%"}},"className":"is-style-rounded"} -->
-<figure class="wp-block-image size-thumbnail has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/headshot.png' ); ?>" alt="" class="wp-image-500" style="border-radius:100%"/></figure>
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:image {"id":303,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100%"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/image-3.jpg' ); ?>" alt="" class="wp-image-303" style="border-radius:100%"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"75%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:75%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
-<h2 class="wp-block-heading" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><?php esc_html_e( 'Mario Peterson', 'jadro' ); ?></h2>
+<h2 class="wp-block-heading" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Maria Peterson</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><?php esc_html_e( 'Corporate Trainer', 'jadro' ); ?></p>
+<p class="has-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px">Corporate Lawyer</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
